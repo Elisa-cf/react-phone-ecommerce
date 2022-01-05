7994c8bbca20aeab622c86e885d8e297b800d272
Elisa <elisacanyelles@gmail.com>
2022-01-05 19:11:40 +0100

    created cartColumns.js and added to cart.js

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import Title from '../Title'
+import CartColumns from './CartColumns'
+
 export default class Cart extends Component {
  render() {
   return (
    <div>
     <Title name="Your" title="cart"/>
+    <CartColumns/>
+
    </div>
   )
  }
